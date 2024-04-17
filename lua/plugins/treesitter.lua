1e3e47d7dd261f0d492ab8936e08c7e194d81aa4
AndresGR02 <andres.gr.workspace@gmail.com>
2024-04-17 17:57:22 -0600

    Add lualine config
    Add a status line with the lua line plugin

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,11 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+        require("nvim-treesitter").setup({
+              ensure_installed = { 
+                  "c", "lua", "vim", 
+                  "vimdoc", "query", "cpp",
+                  "go", } 
+        })
+    end,
+}
